c281377eab1dcda27f5251b91a513f82d0f63835
Jenya Trofimov <deadrime@yandex.ru>
2018-10-06 18:33:07 +0300

    Config for both inline and external SVG

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,17 @@ module.exports = {
     svgRule.uses.clear()
 
     svgRule
+      .oneOf('inline')
+      .resourceQuery(/inline/)
       .use('vue-svg-loader')
       .loader('vue-svg-loader')
+      .end()
+      .end()
+      .oneOf('external')
+      .use('file-loader')
+      .loader('file-loader')
+      .options({
+        name: 'assets/[name].[hash:8].[ext]',
+      })
   },
 }
