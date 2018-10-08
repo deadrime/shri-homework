e3fa094fd21d2d4eec2533a79f796d7dcc8f04b3
Jenya Trofimov <deadrime@yandex.ru>
2018-10-09 00:11:03 +0300

    find rotate

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
   <div class="camera">
     <div
       ref="cameraImage"
@@ -8,14 +9,16 @@
         transform: `scale(${zoomLvl})`,
       }"
       class="camera__image">
-        <div class="camera__navigation">
-          <div class="camera__navigation__current"
-            :style="{
-              left: `calc(${bgPosX}% - 80px)`
-            }"
-          ></div>
-        </div>
+      <div class="camera__navigation">
+        <div 
+          :style="{
+            left: `calc(${bgPosX}% - 80px)`
+          }"
+          class="camera__navigation__current"
+        />
       </div>
+    </div>
+  </div>
   </div>
 </template>
 
@@ -24,121 +27,169 @@ export default {
   data() {
     return {
       bgPosX: 50,
-      zoomLvl: 1
-    };
+      zoomLvl: 1,
+    }
   },
 
   mounted() {
-    const test = this.$refs.cameraImage;
-    const imageW = test.clientWidth;
-    let watchMove = false;
-    let tmpX = 0;
-    let tmpL = 0;
-    let diff = 0;
-    let diffL = 0;
-    let currentGesture = "singleFinger";
-    let pointerEvents = [];
-    let fingers = [];
+    const test = this.$refs.cameraImage
+    const imageW = test.clientWidth
+    let watchMove = false
+    let tmpX = 0
+    let tmpL = 0
+    let diff = 0
+    let diffL = 0
+    let currentGesture = 'singleFinger'
+    let pointerDownEvents = []
+    let pointerMoveEvents = []
+    let fingers = []
+    console.log(test, imageW)
+
+    const isRotate = () => {
+      const dx1 = pointerMoveEvents[0].clientX - pointerDownEvents[0].clientX
+      const dy1 = pointerMoveEvents[0].clientY - pointerDownEvents[0].clientY
+      const dx2 = pointerMoveEvents[1].clientX - pointerDownEvents[1].clientX
+      const dy2 = pointerMoveEvents[1].clientY - pointerDownEvents[1].clientY
+
+      if (Math.abs(dx1) > Math.abs(dy1) && Math.abs(dx2) > Math.abs(dy2)) {
+        if (dx1 >= 2.0 && dx2 <= -2.0) {
+          return true
+        } else if (dx1 <= -2.0 && dx2 >= 2.0) {
+          return true
+        }
+      } else {
+        if (dy1 >= 2.0 && dy2 <= -2.0) {
+          return true
+        } else if (dy1 <= -2.0 && dy2 >= 2.0) {
+          return true
+        }
+      }
+      return false
+    }
 
     const gesturesHandler = {
       singleFinger: {
         pointerdown: e => {
-          watchMove = true;
-          tmpX = e.clientX - diff;
+          watchMove = true
+          tmpX = e.clientX - diff
         },
         pointerup: e => {
-          watchMove = false;
+          watchMove = false
         },
         pointermove: e => {
-          if (!watchMove) return;
-          diff = e.clientX - tmpX;
-          this.bgPosX = 50 - diff / imageW * 100;
+          if (!watchMove) return
+          diff = e.clientX - tmpX
+          this.bgPosX = 50 - diff / imageW * 100
         }
       },
       pinch: {
         pointerdown: ([firstFinger, secondFinger]) => {
-          const x1 = firstFinger.clientX;
-          const y1 = firstFinger.clientY;
-          const x2 = secondFinger.clientX;
-          const y2 = secondFinger.clientY;
-          tmpL = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2) - diffL;
+          const x1 = firstFinger.clientX
+          const y1 = firstFinger.clientY
+          const x2 = secondFinger.clientX
+          const y2 = secondFinger.clientY
+          tmpL = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)) - diffL
         },
         pointerup: e => {
           //console.log(e.pointerId, e.clientX)
         },
         pointermove: ([firstFinger, secondFinger]) => {
-          const x1 = firstFinger.clientX;
-          const y1 = firstFinger.clientY;
-          const x2 = secondFinger.clientX;
-          const y2 = secondFinger.clientY;
+          const x1 = firstFinger.clientX
+          const y1 = firstFinger.clientY
+          const x2 = secondFinger.clientX
+          const y2 = secondFinger.clientY
 
-          const l = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
-          diffL = l - tmpL;
-          this.zoomLvl = Math.max(1, 1 + diffL / imageW * 3);
+          const l =
+            Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)) - diffL
+          diffL = l - tmpL
+          this.zoomLvl = Math.max(1, 1 + diffL / imageW * 3)
         }
       }
-    };
+    }
 
     const findGesture = () => {
-      if (pointerEvents.length === 2) {
-        return "pinch";
-      } else if (pointerEvents.length === 1) {
-        return "singleFinger";
+      if (pointerDownEvents.length === 2 && pointerMoveEvents.length === 2) {
+        if (isRotate()) return 'rotate'
       }
-    };
+    }
 
-    test.addEventListener("touchmove", e => e.preventDefault());
+    // test.addEventListener('touchmove', e => e.preventDefault())
+    test.addEventListener('touchmove', e => {
+      e.preventDefault()
+    })
 
-    test.addEventListener("pointerdown", e => {
-      test.setPointerCapture(e.pointerId);
-      pointerEvents.push(e);
-      currentGesture = findGesture();
-      e.preventDefault();
-      if (currentGesture === "singleFinger") {
-        gesturesHandler["singleFinger"].pointerdown(e);
-      } else if (currentGesture === "pinch") {
-        const findedEvent = pointerEvents.find(
-          i => i.pointerId === e.pointerId
-        );
-        const index = pointerEvents.indexOf(findedEvent);
-        if (index !== -1) {
-          pointerEvents[index] = e;
-          if (pointerEvents.length === 2) {
-            gesturesHandler["pinch"].pointerdown(pointerEvents);
-          }
-        }
-      }
-      return false;
-    });
+    test.addEventListener('pointerdown', e => {
+      // test.setPointerCapture(e.pointerId);
+      pointerDownEvents.push(e)
 
-    test.addEventListener("pointerup", e => {
-      test.releasePointerCapture(e.pointerId);
+      // if (currentGesture === "singleFinger") {
+      //   gesturesHandler["singleFinger"].pointerdown(e);
+      // } else if (currentGesture === "pinch") {
+      //   const findedEvent = pointerEvents.find(
+      //     i => i.pointerId === e.pointerId
+      //   );
+      //   const index = pointerEvents.indexOf(findedEvent);
+      //   if (index !== -1) {
+      //     pointerLastEvents.push(pointerEvents[index])
+      //     pointerEvents[index] = e;
+      //     if (pointerEvents.length === 2) {
+      //       gesturesHandler["pinch"].pointerdown(pointerEvents);
+      //     }
+      //   }
+      // }
+      // return false;
+    })
 
-      pointerEvents = pointerEvents.filter(i => i.pointerId !== e.pointerId);
+    test.addEventListener('pointerup', e => {
+      // test.releasePointerCapture(e.pointerId);
+      pointerDownEvents = pointerDownEvents.filter(
+        i => i.pointerId !== e.pointerId
+      )
+      pointerMoveEvents = pointerMoveEvents.filter(
+        i => i.pointerId !== e.pointerId
+      )
+
       if (currentGesture) {
-        gesturesHandler[currentGesture].pointerup(e);
+        gesturesHandler[currentGesture].pointerup(e)
       }
-    });
+    })
 
-    test.addEventListener("pointermove", e => {
-      e.preventDefault();
-      if (currentGesture === "singleFinger") {
-        gesturesHandler["singleFinger"].pointermove(e);
-      } else if (currentGesture === "pinch") {
-        const findedEvent = pointerEvents.find(
-          i => i.pointerId === e.pointerId
-        );
-        const index = pointerEvents.indexOf(findedEvent);
-        if (index !== -1) {
-          pointerEvents[index] = e;
-          if (pointerEvents.length === 2) {
-            gesturesHandler["pinch"].pointermove(pointerEvents);
+    test.addEventListener('pointermove', e => {
+      e.preventDefault()
+
+      console.log(e)
+
+      if (pointerDownEvents.length >= 2) {
+        if (pointerDownEvents.find(i => i.pointerId === e.pointerId)) {
+          const findedMove = pointerMoveEvents.find(
+            i => i.pointerId === e.pointerId
+          )
+          if (!findedMove) {
+            pointerMoveEvents.push(e)
+          } else {
+            pointerMoveEvents[pointerMoveEvents.indexOf(findedMove)] = e
           }
         }
+        const gesture = findGesture()
+
+        if (gesture === 'rotate') {
+          const dx1 =
+            pointerMoveEvents[0].clientX - pointerDownEvents[0].clientX
+          const dy1 =
+            pointerMoveEvents[0].clientY - pointerDownEvents[0].clientY
+          const dx2 =
+            pointerMoveEvents[1].clientX - pointerDownEvents[1].clientX
+          const dy2 =
+            pointerMoveEvents[1].clientY - pointerDownEvents[1].clientY
+
+          const cX = (pointerMoveEvents[0].clientX - pointerDownEvents[1].clientX)/2
+          const cY = (pointerMoveEvents[0].clientY - pointerDownEvents[1].clientY)/2       
+        }
       }
-    });
+    })
   }
-};
+
+}
 </script>
 
 <style lang="stylus" scoped>
@@ -160,15 +211,15 @@ export default {
     width: 80%;
     height: 60px;
     background: rgba(255, 255, 255, 0.2);
-    border-radius 8px
-    overflow hidden
+    border-radius: 8px;
+    overflow: hidden;
 
     &__current {
-      position absolute
-      border-radius 8px
-      width 160px
-      height 100%
-      background rgba(0, 0, 0, 0.5);
+      position: absolute;
+      border-radius: 8px;
+      width: 160px;
+      height: 100%;
+      background: rgba(0, 0, 0, 0.5);
     }
   }
 }
