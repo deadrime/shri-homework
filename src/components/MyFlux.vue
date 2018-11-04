95a3a4f5ed6714fae91079a3ac63399ff00395b9
Jenya Trofimov <deadrime@yandex.ru>
2018-11-05 02:50:49 +0300

    Use my flux implementation

---
+++ src/components/MyFlux.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1> Счетчик: {{counter}} </h1>
+    <button @click="increment">+1</button>
+    <button @click="decrement">-1</button>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Store from '@/store'
+
+export default Vue.extend({
+  data() {
+    return {
+      counter: null
+    }
+  },
+  created() {
+    this.counter = Store.state.counter
+    Store.subscribe(state => {
+      this.counter = state.counter
+    })
+  },
+  methods: {
+    increment() {
+      Store.run('increment')
+    },
+    decrement() {
+      Store.run('decrement')
+    }
+  }
+})
+</script>
