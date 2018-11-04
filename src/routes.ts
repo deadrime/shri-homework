bf0790036de8a84c6c192bae1a89d32e15d2ac77
Jenya Trofimov <deadrime@yandex.ru>
2018-11-05 02:51:11 +0300

    Add route

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,14 @@ import VueRouter from 'vue-router'
 import Events from '@/components/Events.vue'
 import Camera from '@/components/Camera.vue'
 import Live from '@/components/Live.vue'
+import Flux from '@/components/MyFlux.vue'
 
 const routes = [
   { path: '/', redirect: '/events' },
   { path: '/events', component: Events },
   { path: '/camera', component: Camera},
   { path: '/live', component: Live},
+  { path: '/flux', component: Flux}
 ]
 
 const router = new VueRouter({
