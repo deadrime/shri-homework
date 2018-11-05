d2428342dcde156721ed44bfae1bc87edd31115e
Jenya Trofimov <deadrime@yandex.ru>
2018-11-05 04:56:43 +0300

    Add store as dependency, save data in localstore

--- src/store.ts
+++ src/store.ts
@@ -1,68 +1,18 @@
-const cloneDeep = require('lodash/cloneDeep')
-
-interface State {
-  [key: string]: any
-}
-
-type Mutation = ({state}: {state: State}, args?: any) => any
-
-type Listener = ((newState: any, prevState: any) => any)
-
-interface Mutations {
-  [method: string]: Mutation
-}
-
-interface Reducer {
-  state: State
-  mutations: Mutations
-}
-
-class Store {
-  public state: State
-  private mutations: Mutations
-  private listeners: Listener[]
-
-  constructor({ state, mutations }: {state: State, mutations: Mutations}) {
-    this.mutations = mutations
-    this.state = state
-    this.listeners = []
-  }
-
-  run(action: string, ...args: any): any {
-    const prevState = this.state
-    const newState = cloneDeep(this.state)
-    const result = this.mutations[action]({ state: newState }, args)
-    this.state = newState
-    this.listeners.forEach(cb => {
-      cb(newState, prevState)
-    })
-    return result
-  }
-
-  getState(): State {
-    return cloneDeep(this.state)
-  }
-
-  subscribe(newListener: Listener): void {
-    this.listeners.push(newListener)
-  }
-
-  unsubscribe(cb: () => {}): void {
-    this.listeners.splice(this.listeners.indexOf(cb), 1)
-  }
-}
+import {Store, Reducer} from "store"
 
 
 const myStore: Reducer = {
   state: {
-    counter: 0,
+    counter: localStorage.getItem('counter') || 0,
   },
   mutations: {
     increment({ state }) {
       state.counter += 1
+      localStorage.setItem('counter',state.counter)
     },
     decrement({ state }) {
       state.counter -= 1
+      localStorage.setItem('counter',state.counter)
     }
   }
 }
